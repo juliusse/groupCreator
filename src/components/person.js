90e7a6f4a19d4bd8fdbd56d52337b8a81d34fc3b
Julius Seltenheim <mail@julius-seltenheim.com>
2017-12-02 21:56:40 +0100

    correct name displaying

--- src/components/person.js
+++ src/components/person.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import _ from 'lodash';
 import './person.less';
 
+function getLastNameInitials(lastName) {
+  if (_.isEmpty(lastName)) {
+    return '';
+  }
+
+  return lastName
+    .split('-')
+    .map(part => `${part[0]}.`)
+    .join('-');
+}
+
 export function Person({ name, gender, isGroupLeader }) {
-  const firstName = name.substring(0, name.indexOf(' '));
-  const lastNameInitial = name[name.lastIndexOf(' ') + 1];
-  const displayName = `${firstName} ${lastNameInitial}.`;
+  const nameParts = name.split(' ');
+
+  const firstName = _.first(nameParts);
+  const lastName = nameParts.length > 1 ? _.last(nameParts) : '';
+  const displayName = `${firstName} ${getLastNameInitials(lastName)}`;
 
   return (
     <div className="person" data-gender={gender}>
