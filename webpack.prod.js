c251a8980f23aec03512a7815e56ff5a43b07590
Julius Seltenheim <mail@julius-seltenheim.com>
2017-12-02 12:55:06 +0100

    linter

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,7 +4,7 @@ const common = require('./webpack.common.js');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = merge(common, {
-    plugins: [
-        new UglifyJSPlugin()
-    ]
+  plugins: [
+    new UglifyJSPlugin(),
+  ],
 });
